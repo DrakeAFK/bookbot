694090ae767f427d0e5723347493acd3589e5f7d
DrakeAFK <drakehopkins@outlook.com>
2024-03-09 17:55:20 -0500

    begin last step of project

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello world")
+def main():
+  book_path = "books/frankenstein.txt"
+  text = get_book_text(book_path)
+  num_words = get_num_words(text)
+  letters = get_letter_count(text)
+  print(f"--- Begin report of {book_path} ---")
+
+def get_num_words(text):
+  words = text.split()
+  return len(words)
+
+def get_book_text(path):
+  with open(path) as f:
+    return f.read()
+
+def get_letter_count(text):
+  letter_count = {}
+  for letter in text:
+    lowered = letter.lower()
+    if lowered in letter_count:
+      letter_count[lowered] += 1
+    else:
+      letter_count[lowered] = 1
+  return letter_count
+
+main()
