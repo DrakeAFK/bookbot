4f02cb37f2e81f67fff62d29bf99796f2e0505e2
DrakeAFK <drakehopkins@outlook.com>
2024-03-09 19:33:18 -0500

    finish the project

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def main():
   text = get_book_text(book_path)
   num_words = get_num_words(text)
   letters = get_letter_count(text)
+  sorted_letters = sort_letters(letters)
   print(f"--- Begin report of {book_path} ---")
+  print(f"{num_words} words found in the document\n")
+  for letter in sorted_letters:
+    print(f"The '{letter['letter']}' character was found {letter['count']} times")
+  print("--- End report ---")
 
 def get_num_words(text):
   words = text.split()
@@ -17,10 +22,19 @@ def get_letter_count(text):
   letter_count = {}
   for letter in text:
     lowered = letter.lower()
-    if lowered in letter_count:
-      letter_count[lowered] += 1
-    else:
-      letter_count[lowered] = 1
+    if lowered.isalpha():
+      if lowered in letter_count:
+        letter_count[lowered] += 1
+      else:
+        letter_count[lowered] = 1
   return letter_count
+
+def sort_letters(dict):
+  letter_list = [
+    {'letter': key, 'count': value} for key, value in dict.items()
+  ]
+  sorted_letters = sorted(letter_list, key=lambda x: x['count'], reverse=True)
+  return sorted_letters
+
 
 main()
